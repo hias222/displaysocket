7bb0f31527ff4eb74a3a19d7ac2a250e857a035c
Matthias Fuchs <matthias.fuchs@devtops.gmbh>
2020-11-29 21:20:55 +0100

    remove old file

--- outgoing/senddatahub.js
+++ outgoing/senddatahub.js
@@ -1,5 +1,7 @@
 global.fetch = require("node-fetch");
 
+var use_backuend_url = process.env.APP_USE_DATAHUB === "true" ? true : false
+
 var get_backend_port = process.env.APP_BACKEND_PORT === undefined ? "3001" : process.env.APP_BACKEND_PORT
 var get_backend_url = process.env.APP_BACKEND_DIRECT === "true" ? "http://localhost:" + get_backend_port : process.env.APP_DATHUB
 var backend_url = get_backend_url === undefined ? "http://localhost:" + get_backend_port : get_backend_url
@@ -7,34 +9,39 @@ var backend_url = get_backend_url === undefined ? "http://localhost:" + get_back
 console.log(backend_url)
 
 const sendHeat = (jsondaata) => {
-    console.log('sendheat to ' + backend_url +  '/api/heat/add')
-    let responsedata = "OK"
+    if (!use_backuend_url){
+        console.log('datahub is off')
+    }
+    if (use_backuend_url) {
+        console.log('sendheat to ' + backend_url + '/api/heat/add')
+        let responsedata = "OK"
 
-    console.log(JSON.stringify(jsondaata))
+        console.log(JSON.stringify(jsondaata))
 
-    fetch(backend_url + '/api/heat/add', {
-        method: 'POST', // or 'PUT'
-        headers: {
-            'Content-Type': 'application/json'
-            // 'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        cache: 'no-cache',
-        body: JSON.stringify(jsondaata)
-    })
-        .then(response => {
-            responsedata = response;
-            return response
+        fetch(backend_url + '/api/heat/add', {
+            method: 'POST', // or 'PUT'
+            headers: {
+                'Content-Type': 'application/json'
+                // 'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            cache: 'no-cache',
+            body: JSON.stringify(jsondaata)
         })
-        .then(data => {
-            console.log('Success:', data);
-        })
-        .catch((error) => {
-            console.error('Error:', error);
-            console.log('jsondaata: ');
-            console.log(JSON.stringify(jsondaata))
-            console.log('responsedata: ');
-            console.log(responsedata)
-        });
+            .then(response => {
+                responsedata = response;
+                return response
+            })
+            .then(data => {
+                console.log('Success:', data);
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+                console.log('jsondaata: ');
+                console.log(JSON.stringify(jsondaata))
+                console.log('responsedata: ');
+                console.log(responsedata)
+            });
+    }
 }
 
 exports.sendHeat = sendHeat;
--- sendDataHub.js
+++
@@ -1,8 +0,0 @@
-const { headermessage, lanemessages } = require("./app");
-function sendDataHub() {
-  console.log("send to datahub");
-  console.log("header");
-  console.log(headermessage);
-  console.log("lanes");
-  console.log(lanemessages);
-}
